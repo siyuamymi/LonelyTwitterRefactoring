761104187194877e3430891f2f72188b4c0ddc6b
siyuamymi <siyuamymi@gmail.com>
2019-03-20 17:20:49 -0600

    finished lab 9

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright <YEAR> <COPYRIGHT HOLDER>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package ca.ualberta.cs.lonelytwitter;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Date;
+
+abstract class Tweet implements Serializable {
+    private static final long serialVersionUID = 1L;
+    protected Date tweetDate;
+    protected String tweetBody;
+
+    public Date getTweetDate() {
+        return tweetDate;
+    }
+
+    public void setTweetDate(Date tweetDate) {
+        this.tweetDate = tweetDate;
+    }
+
+    public String getTweetBody() {
+        return tweetBody;
+    }
+
+    public void setTweetBody(String tweetBody) {
+        this.tweetBody = tweetBody;
+    }
+
+    private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+        out.writeObject(tweetDate);
+        out.writeObject(tweetBody);
+    }
+
+    private void readObject(java.io.ObjectInputStream in) throws IOException,
+            ClassNotFoundException {
+        tweetDate = (Date) in.readObject();
+        tweetBody = (String) in.readObject();
+    }
+
+    @Override
+    public String toString() {
+        return getTweetDate() + " | " + getTweetBody() ;
+    }
+
+    public abstract boolean isValid();
+}
